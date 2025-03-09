c4bdb35b78633dd56fce698f5d85b4aa13fa83a6
andresiriarts <andresiriarte.se@gmail.com>
2025-03-09 02:13:47 -0300

    Creacion de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-/* import Home from './assets/components/Home' */
 import Footer from './assets/components/Footer'
-import Login from './assets/components/Login'
+import Login from './assets/pages/Login'
 import Navbar from './assets/components/Navbar'
-import Register from './assets/components/Register'
-import Home from './assets/components/Home'
-import Cart from './assets/components/Cart'
-import Pizza from './assets/components/Pizza'
+import Register from './assets/pages/Register'
+import Home from './assets/pages/Home'
+import Cart from './assets/pages/Cart'
+import Pizza from './assets/pages/Pizza'
+/* import Profile from './assets/pages/Profile' */
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Home/>
-      {/* <Pizza/> */}
-      {/* <Cart/> */}
-      {/* <Register /> */}
-      {/* <Login /> */}
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/pizza/p001' element={<Pizza />} />
+          {/* <Route path='/profile' element={<Profile />} /> */}
+          <Route path='/404' element={<Home />} />
+          
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </>
   )
